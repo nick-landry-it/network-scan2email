81bfd07d5269792b0adf0181ea4cd7ad11c3be40
Nick Landry <nicklandryit@gmail.com>
2022-03-04 12:29:08 -0600

    Initial Program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ipupdater
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"ipupdater/cryptography"
+	"ipupdater/networking"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Println(e)
+		panic(e)
+	}
+}
+
+func main() {
+	encryptedEmail, err := os.ReadFile("./encryptedfiles/.encryptedEmail.lck")
+	check(err)
+	encryptedPassword, err := os.ReadFile("./encryptedfiles/.encryptedPassword.lck")
+	check(err)
+	encryptionKey, err := os.ReadFile("./encryptedfiles/.encryptionKey.lck")
+	check(err)
+
+	ipPlaceholder := networking.GetNetwork()
+	fmt.Println(ipPlaceholder)
+	decryptedEmail := cryptography.Decrypt(string(encryptedEmail), string(encryptionKey))
+	decryptedPassword := cryptography.Decrypt(string(encryptedPassword), string(encryptionKey))
+	fmt.Printf("Decrypted Email : %s\n", decryptedEmail)
+	fmt.Printf("Decrypted Password : %s\n", decryptedPassword)
+}
